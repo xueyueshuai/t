c17dbc258f04ed3a2b7a950938dd0c0621da8839
xys <xueyueshuai@xinzhidi.com>
2022-05-24 13:28:14 +0800

    a

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ npm run build
 点击查看 [在线演示](https://xueyueshuai.github.io/t/web_dist)
 
 ##  todo-list
-- 中英文
+- 中英文 vue中使用i18n
 - 自定义 v-model
-- form验证每一项 （自定义的项）
+- form 验证每一项 （自定义的项）
 - extend 全局弹框
---
+++ src/views/example/formJ/code.vue
@@ -0,0 +1,55 @@
+<template>
+  <a-form-model ref="ruleForm" :model="ruleForm" :rules="rules" v-bind="layout">
+    <a-form-model-item label="username" prop="username">
+       <input type="text">
+    </a-form-model-item>
+    <a-form-model-item label="password" prop="password">
+      <a-input v-model="ruleForm.password" type="password" autocomplete="off" />
+    </a-form-model-item>
+  
+    <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click="submitForm('ruleForm')">
+        Submit
+      </a-button>
+      <a-button style="margin-left: 10px" @click="resetForm('ruleForm')">
+        Reset
+      </a-button>
+    </a-form-model-item>
+  </a-form-model>
+</template>
+<script>
+export default {
+  data() {
+    
+    return {
+      ruleForm: {
+        username: '',
+        password: '',
+      },
+      rules: {
+        username: [{ required: true, trigger: 'change' }],
+        password: [{ required: true, trigger: 'change' }],
+      },
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 },
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
