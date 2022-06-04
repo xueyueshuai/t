225ae5987bc67ae670b38f43b5aca0c05b6945f0
xys <xueyueshuai@xinzhidi.com>
2022-06-04 14:49:06 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <xysLayout />
+    <!-- <xysLayout /> -->
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ Vue.use(VueRouter)
 let routes = [
   { path: '/home', name: 'Home', component: () => import('@/views/Home.vue') },
   { path: '/about', name: 'About', component: () => import('@/views/About.vue') },
+  { path: '/login', name: 'login', component: () => import('@/views/login.vue') },
 ]
 
 const router = new VueRouter({
@@ -46,7 +47,7 @@ router.beforeEach((to, from, next) => {
           router.addRoute({
             path: '/',
             redirect: '/test/xys/test1',
-            component: () => import('@/components/xysLayout/emptyLayout.vue'),
+            component: () => import('@/components/xysLayout/xysLayout.vue'),
             children: n
           })
 
@@ -63,7 +64,7 @@ router.beforeEach((to, from, next) => {
           console.log("finally");
         });
     }
-  } else if ([].includes(to.name)) {
+  } else if (['login'].includes(to.name)) {
     next();
   } else {
     next({
