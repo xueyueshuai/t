18fb48bed9104697ebae3a9983345bfbb99ddc38
xys <xueyueshuai@xinzhidi.com>
2022-06-04 14:37:24 +0800

    a

--- src/views/test/xys/test2.vue
+++ src/views/test/xys/test2.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>xys-test2</div>
+  <div>
+    <h2>xys-test2</h2>
+
+    <el-button @click="onTest">onTest</el-button>
+  </div>
 </template>
 
 <script>
-export default {};
+import xueyueshuaitestpublish from "xueyueshuaitestpublish";
+import "xueyueshuaitestpublish/style.css";
+export default {
+  data() {
+    return {
+      i: 0,
+    };
+  },
+  methods: {
+    onTest() {
+      // xueyueshuaitestpublish.Alert.show()
+      xueyueshuaitestpublish.Toast.show("我就" + this.i++, "error", 3000);
+    },
+  },
+};
 </script>
+
 
 <style lang="less" scoped>
 </style>
